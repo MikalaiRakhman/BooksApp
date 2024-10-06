1ebd30c39abe44928eabc3d3acb320c64fbe0b8d
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-10-06 15:27:53 +0200

    Changes: -BooksController ViewModel -BookViewModel -Books --Create --Delete --Edit --Index

---
+++ BooksApp/ViewModel/BookViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BooksApp.ViewModel
+{
+    public class BookViewModel
+    {
+        string Name { get; set; }
+        string Author { get; set; }
+        int Year { get; set; }
+
+    }
+}
