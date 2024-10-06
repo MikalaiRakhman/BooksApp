5f846f818f1818cc5f96a1d9689da2fd40721c76
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-10-06 18:13:20 +0200

    changes in Vews.Books and in Controller.BooksController. add id to the BookViewModel

--- BooksApp/Controllers/BooksController.cs
+++ BooksApp/Controllers/BooksController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BooksApp.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BooksApp.Controllers
 {
     public class BooksController : Controller
     {
+        private static List<BookViewModel> _bookViewModels = 
+            [
+            new BookViewModel {Id = 1, Name = "Johnny Mneminic", Author = "William Gibson", Year = 1981 },
+            new BookViewModel {Id = 2, Name = "Dune", Author = "Frank Herbert", Year = 1963},
+            new BookViewModel {Id = 3, Name = "The Witcher", Author = "Andrzej Sapkowski", Year = 1990}
+            ];
+
         public IActionResult Index()
         {
-            return View();
+            ViewBag.AnotherTitle = "List of books";       
+            var bookViewModels = _bookViewModels;
+
+            return View(bookViewModels);
         }
 
         public IActionResult Create()
@@ -14,14 +25,66 @@ namespace BooksApp.Controllers
             return View();
         }
 
-        public IActionResult Edit()
+        [HttpPost]        
+        public IActionResult Create([FromForm] BookViewModel book)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _bookViewModels.Add(book);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(book);
         }
 
-        public IActionResult Delete()
+        public IActionResult Edit(int id)
         {
-            return View();
+            var book = _bookViewModels.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(BookViewModel book)
+        {
+            var existingBook = _bookViewModels.FirstOrDefault(b => b.Id == book.Id);
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+
+            existingBook.Name = book.Name;
+            existingBook.Author = book.Author;
+            existingBook.Year = book.Year;
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var book = _bookViewModels.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var book = _bookViewModels.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookViewModels.Remove(book);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- BooksApp/ViewModel/BookViewModel.cs
+++ BooksApp/ViewModel/BookViewModel.cs
@@ -1,10 +1,21 @@
-﻿namespace BooksApp.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksApp.ViewModel
 {
     public class BookViewModel
     {
-        string Name { get; set; }
-        string Author { get; set; }
-        int Year { get; set; }
+        public int Id {  get; set; }
 
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name can't be longer than 50 characters.")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Author is required.")]
+        [StringLength(50, ErrorMessage = "Author can't be longer than 50 characters.")]
+        public required string Author { get; set; }
+
+        [Required(ErrorMessage = "Year is required.")]
+        [Range(1800, 2025, ErrorMessage = "Year must be between 1800 and 2025.")]
+        public int Year { get; set; }
     }
 }
